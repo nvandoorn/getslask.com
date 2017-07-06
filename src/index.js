947318e33c247577495be4f7eb1a7f6b3d168dc1
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-07-05 22:38:16 -0700

    Update import order so css stays together in build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Layout from './components/layout/layout'
+
 import  './index.css'
 import '../node_modules/font-awesome/css/font-awesome.css'
 import '../fonts/beachbar-bold/beachbar-bold.css'
-
-import Layout from './components/layout/layout'
 
 ReactDOM.render(<Layout />, document.getElementById('root'))
 
